ecdc08033dba05edd0017b3f2cca3e74a7bcc6b1
Mcorduk <om.corduk@gmail.com>
2025-03-14 14:41:58 +0300

    feat: add getDays helper to get formatted dates dynamically

--- src/helpers/helper.ts
+++ src/helpers/helper.ts
@@ -11,7 +11,7 @@ export function cityFromAdress(address: string) {
 
 export function formatTimeLabel(
   timeString: string,
-  forecastType: "hourly" | "weekly" // Literal union type
+  forecastType: "hourly" | "weekly"
 ): string {
   if (!timeString) return "";
 
@@ -20,10 +20,35 @@ export function formatTimeLabel(
     return format(date, "hh a");
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
   if (forecastType === "weekly") {
     const date = parseISO(timeString);
     return isToday(date) ? "Now" : format(date, "EEE");
   }
 
   return timeString;
+}
+
+/*
+  from: how many days from today, can take negative
+  to: how many days from "from" date
+*/
+export function getDays(from = 0, to = 3) {
+  const days = [];
+
+  if (!Number.isInteger(from) || !Number.isInteger(to))
+    throw new Error("Both 'from' and 'to' parameters must be integers.");
+
+  if (to < 0) throw new Error("'to' parameter must be a non-negative integer.");
+
+  for (let i = 0; i < to; i++) {
+    const day = new Date();
+    day.setDate(new Date().getDate() + i + from);
+
+    const formattedDay = day.toISOString().split("T")[0];
+
+    days.push(formattedDay);
+  }
+
+  return days;
 }
