d0333d8c5d9b36efaa3a0e5137bd4fa3fcb84624
Mcorduk <om.corduk@gmail.com>
2025-03-19 11:08:27 +0300

    feat: add helper to capitalize letter

--- src/helpers/helper.ts
+++ src/helpers/helper.ts
@@ -1,34 +1,3 @@
-import { format, isToday, parse, parseISO } from "date-fns";
-
-export function cityFromAdress(address: string) {
-  const commaIndex = address.indexOf(",");
-  if (commaIndex === -1) {
-    return address;
-  }
-  const city = address.substring(0, commaIndex);
-  return city;
-}
-
-export function formatTimeLabel(
-  timeString: string,
-  forecastType: "hourly" | "weekly"
-): string {
-  if (!timeString) return "";
-
-  if (forecastType === "hourly") {
-    const date = parse(timeString, "HH:mm:ss", new Date());
-    return format(date, "hh a");
-  }
-
-  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-  if (forecastType === "weekly") {
-    const date = parseISO(timeString);
-    return isToday(date) ? "Now" : format(date, "EEE");
-  }
-
-  return timeString;
-}
-
 /*
   from: how many days from today, can take negative
   to: how many days from "from" date
@@ -51,4 +20,8 @@ export function getDays(from = 0, to = 3) {
   }
 
   return days;
+}
+
+export function capitalizeFirstLetter(str: string) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
 }
