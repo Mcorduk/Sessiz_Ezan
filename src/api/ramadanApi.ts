b3101eeb8f79ecb91af2e4375469b7abc669ffa7
Mcorduk <om.corduk@gmail.com>
2025-04-03 20:22:27 +0300

    refactor: try to bypass cors error by using a cors proxy

--- src/api/ramadanApi.ts
+++ src/api/ramadanApi.ts
@@ -2,7 +2,8 @@ import { RamadanResponse } from "../types/ramadanApi";
 
 export async function checkRamadan(): Promise<boolean> {
   try {
-    const baseUrl = "https://ramadan.munafio.com/api/check";
+    const baseUrl =
+      "https://workers-playground-morning-butterfly-7687.o-muhammetcorduk.workers.dev/https://ramadan.munafio.com/api/check";
 
     const response = await fetch(baseUrl, { mode: "cors" });
 
@@ -12,7 +13,10 @@ export async function checkRamadan(): Promise<boolean> {
 
     const parsedResponse = (await response.json()) as RamadanResponse;
 
-    return parsedResponse.data.isRamadan;
+    const isRamadan = parsedResponse.data.isRamadan;
+    console.log("Is it Ramadan?: ", isRamadan);
+
+    return isRamadan;
   } catch (error) {
     throw new Error(`Ramadan API Error + ${error}`);
   }
